7508a8dd5fc499ce78eb75f59155a3fc2f55d2d3
93geraldtee <93geraldtee@gmail.com>
2020-03-21 17:32:12 -0400

    add validation and accept nested attributes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,13 @@
 class Recipe < ApplicationRecord
+
     belongs_to :user
     has_many :recipe_ingredients
     has_many :ingredients, through: :recipe_ingredients
+
+    validates :name, presence: true
+    
+
+    accepts_nested_attributes_for :ingredients
+    accepts_nested_attributes_for :recipe_ingredients
+
 end
