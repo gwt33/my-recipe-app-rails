d78bd0f234d625da5ac1150ba603d4b75e5ea678
93geraldtee <93geraldtee@gmail.com>
2020-03-21 18:19:36 -0400

    add scope method for order

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,8 +6,16 @@ class Recipe < ApplicationRecord
 
     validates :name, presence: true
     
-
     accepts_nested_attributes_for :ingredients
     accepts_nested_attributes_for :recipe_ingredients
 
+    def self.sort_by_asc_order
+        order(:created_at)
+    end
+
+    def self.sort_by_desc_order
+        order(created_at: :desc)
+    end
+
+    # def self.
 end
