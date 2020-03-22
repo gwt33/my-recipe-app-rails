4e91bd1a84a86e0b7375c9ae85db0951f295686f
93geraldtee <93geraldtee@gmail.com>
2020-03-21 20:49:05 -0400

    run rake db:migrate

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -6,5 +6,6 @@ class Ingredient < ApplicationRecord
     has_many :recipes, through: :recipe_ingredients
 
     validates :name, presence: true, uniqueness: true
-
+    
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_20_201741) do
+ActiveRecord::Schema.define(version: 2020_03_21_170735) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "ingredient_id"
+    t.integer "quantity", default: 1
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ingredient_id"], name: "index_recipe_ingredients_on_ingredient_id"
+    t.index ["recipe_id"], name: "index_recipe_ingredients_on_recipe_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "directions"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
